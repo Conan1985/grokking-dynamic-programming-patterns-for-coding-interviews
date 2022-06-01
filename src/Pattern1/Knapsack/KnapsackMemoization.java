9fc7248db9bdd2c40895c3f2db75ddfc9e22e6fd
Conan1985 <Conan1985@users.noreply.github.com>
2022-06-01 13:19:16 -0500

    Memorization solution to 0/1 Knapsack.

---
+++ src/Pattern1/Knapsack/KnapsackMemoization.java
@@ -0,0 +1,39 @@
+package Pattern1.Knapsack;
+
+class KnapsackMemoization {
+
+    public int solveKnapsack(int[] profits, int[] weights, int capacity) {
+        // TODO: Write your code here
+        if (capacity <= 0 || profits.length <= 0 || profits.length != weights.length) {
+            return 0;
+        }
+        int n = profits.length;
+        Integer[][] dp = new Integer[n][capacity + 1];
+        return solveKnapsackRecursive(dp, profits, weights, capacity, 0);
+    }
+
+    private int solveKnapsackRecursive(Integer[][] dp, int[] profits, int[] weights, int capacity, int currentIndex) {
+        if (currentIndex >= profits.length) {
+            return 0;
+        };
+        if (dp[currentIndex][capacity] != null) {
+            return dp[currentIndex][capacity];
+        }
+        int profit1 = 0;
+        if (weights[currentIndex] <= capacity) {
+            profit1 = profits[currentIndex] + solveKnapsackRecursive(dp, profits, weights, capacity - weights[currentIndex], currentIndex + 1);
+        }
+        int profit2 = solveKnapsackRecursive(dp, profits, weights, capacity, currentIndex + 1);
+        return Math.max(profit1, profit2);
+    }
+
+    public static void main(String[] args) {
+        KnapsackBruteForce ks = new KnapsackBruteForce();
+        int[] profits = {1, 6, 10, 16};
+        int[] weights = {1, 2, 3, 5};
+        int maxProfit = ks.solveKnapsack(profits, weights, 7);
+        System.out.println("Total knapsack profit ---> " + maxProfit);
+        maxProfit = ks.solveKnapsack(profits, weights, 6);
+        System.out.println("Total knapsack profit ---> " + maxProfit);
+    }
+}
