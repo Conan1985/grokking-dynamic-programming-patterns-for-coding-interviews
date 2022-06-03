036d3a1427e278c7ce7d8f017c0d26750561eadd
Conan1985 <Conan1985@users.noreply.github.com>
2022-06-03 10:39:45 -0500

    Tabulation solution to Subset Sum

---
+++ src/Pattern1/SubsetSum/SubsetSumTabulation.java
@@ -0,0 +1,43 @@
+package Pattern1.SubsetSum;
+
+public class SubsetSumTabulation {
+    public boolean canPartition(int[] num, int sum) {
+        if (num.length == 0) {
+            return false;
+        }
+        if (sum == 0) {
+            return true;
+        }
+        int n = num.length;
+        boolean[] dp = new boolean[sum + 1];
+        dp[0] = true;
+        for (int s = 1; s <= sum; s++) {
+            dp[s] = num[0] == s;
+        }
+        for (int i = 1; i < n; i++) {
+            boolean[] oldDP = new boolean[sum + 1];
+            for (int k = 0; k <= sum; k++) {
+                oldDP[k] = dp[k];
+            }
+            for (int s = 1; s <= sum; s++) {
+                if (oldDP[s]) {
+                    dp[s] = oldDP[s];
+                } else if (num[i] <= s) {
+                    dp[s] = oldDP[s - num[i]];
+                }
+            }
+        }
+        return dp[sum];
+    }
+
+    public static void main(String[] args) {
+        SubsetSumTabulation ss = new SubsetSumTabulation();
+        int[] num = { 1, 2, 3, 7 };
+        System.out.println(ss.canPartition(num, 6));
+        num = new int[] { 1, 2, 7, 1, 5 };
+        System.out.println(ss.canPartition(num, 10));
+        num = new int[] { 1, 3, 4, 8 };
+        System.out.println(ss.canPartition(num, 6));
+    }
+
+}
