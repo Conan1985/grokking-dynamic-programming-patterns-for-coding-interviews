06129890d7d351a416f1ae5a123207372becdaa6
Conan1985 <Conan1985@users.noreply.github.com>
2022-06-04 08:22:09 -0500

    Memoization solution to Partition Set

---
+++ src/Pattern1/MinimumSubsetSumDifference/PartitionSetMemoization.java
@@ -0,0 +1,46 @@
+package Pattern1.MinimumSubsetSumDifference;
+
+class PartitionSetMemoization {
+
+    public int canPartition(int[] num) {
+        // TODO: Write your code here
+        if (num.length == 0) {
+            return 0;
+        }
+        if (num.length == 1) {
+            return num[0];
+        }
+        if (num.length == 2) {
+            return Math.abs(num[0] - num[1]);
+        }
+        int n = num.length;
+        int sum = 0;
+        for (int k : num) {
+            sum += k;
+        }
+        Integer[][] dp = new Integer[n][sum + 1];
+        return canPartitionRecursive(dp, num, 0, 0, 0);
+    }
+
+    private int canPartitionRecursive(Integer[][] dp, int[] num, int sum1, int sum2, int currentIndex) {
+        if (currentIndex == num.length) {
+            return Math.abs(sum1 - sum2);
+        }
+        if (dp[currentIndex][sum1] == null) {
+            int diff1 = canPartitionRecursive(dp, num, sum1 + num[currentIndex], sum2, currentIndex + 1);
+            int diff2 = canPartitionRecursive(dp, num, sum1, sum2 + num[currentIndex], currentIndex + 1);
+            dp[currentIndex][sum1] = Math.min(diff1, diff2);
+        }
+        return dp[currentIndex][sum1];
+    }
+
+    public static void main(String[] args) {
+        PartitionSetMemoization ps = new PartitionSetMemoization();
+        int[] num = {1, 2, 3, 9};
+        System.out.println(ps.canPartition(num));
+        num = new int[]{1, 2, 7, 1, 5};
+        System.out.println(ps.canPartition(num));
+        num = new int[]{1, 3, 100, 4};
+        System.out.println(ps.canPartition(num));
+    }
+}
