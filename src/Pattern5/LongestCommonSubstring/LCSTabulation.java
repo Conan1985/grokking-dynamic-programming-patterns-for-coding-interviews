c711ca051bb21acd4e0b56f6b0a60352c5765604
Conan1985 <Conan1985@users.noreply.github.com>
2022-07-30 12:35:07 -0500

    Longest Common Substring

---
+++ src/Pattern5/LongestCommonSubstring/LCSTabulation.java
@@ -0,0 +1,28 @@
+package Pattern5.LongestCommonSubstring;
+
+class LCSTabulation {
+
+    public int findLCSLength(String s1, String s2) {
+        int[][] dp = new int[s1.length()][s2.length()];
+        int maxCount = 0;
+        for (int i1 = 0; i1 < s1.length(); i1++) {
+            for (int i2 = 0; i2 < s2.length(); i2++) {
+                if (s1.charAt(i1) == s2.charAt(i2)) {
+                    if (i1 == 0 || i2 == 0) {
+                        dp[i1][i2] = 1;
+                    } else {
+                        dp[i1][i2] = 1 + dp[i1 - 1][i2 - 1];
+                    }
+                    maxCount = Math.max(maxCount, dp[i1][i2]);
+                }
+            }
+        }
+        return maxCount;
+    }
+
+    public static void main(String[] args) {
+        LCSTabulation lcs = new LCSTabulation();
+        System.out.println(lcs.findLCSLength("abdca", "cbda"));
+        System.out.println(lcs.findLCSLength("passport", "ppsspt"));
+    }
+}
